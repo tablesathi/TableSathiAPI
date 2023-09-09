d457876eaf06dc4de26c5e3dd3c4bb43254a35a1
devgaurav8 <gaurav.dev8354@gmail.com>
2023-09-09 18:41:49 +0530

    Initial commit

---
+++ src/main/java/com/table/sathi/SathiApplication.java
@@ -0,0 +1,13 @@
+package com.table.sathi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SathiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SathiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/table/sathi/SathiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.table.sathi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SathiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
